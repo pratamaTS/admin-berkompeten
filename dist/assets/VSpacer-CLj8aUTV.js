a95e64f04579498feaabc365423bbf16675699cd
unknown <jouga@jojonomic.com>
2024-07-06 19:38:16 +0700

    fix typo

--- dist/assets/VSpacer-CLj8aUTV.js
+++
@@ -1,1 +0,0 @@
-import"./VGrid-CSSzMaaB.js";import{c as r}from"./forwardRefs-Bb7VBe2S.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ dist/assets/VSpacer-D0lJyC4C.js
@@ -0,0 +1,1 @@
+import"./VGrid-CSSzMaaB.js";import{c as r}from"./forwardRefs-BpuzTU2e.js";const e=r("v-spacer","div","VSpacer");export{e as V};
